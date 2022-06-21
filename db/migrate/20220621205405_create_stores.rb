f438a349835fc195870faad53010a9406ba1b865
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2022-06-21 18:26:21 -0300

    Create stores model

---
+++ db/migrate/20220621205405_create_stores.rb
@@ -0,0 +1,11 @@
+class CreateStores < ActiveRecord::Migration[7.0]
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :model
+      t.integer :inventory
+
+      t.timestamps
+    end
+  end
+end
