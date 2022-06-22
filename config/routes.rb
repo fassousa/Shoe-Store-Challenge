df3d8230e457e7198a5e69017a8a9c7b70aeb219
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2022-06-22 01:39:10 -0300

    Add inventory route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :stores
+      get 'inventory', to: 'stores#inventory'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
